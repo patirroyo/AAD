c227bf1b69e9fc197651ed0dbd781d86d85ab253
Alberto Saz <albertosaz@gmail.com>
2023-09-15 12:20:37 +0200

    Empezando a entender algo

--- RazorPages1/RazorPages1/Pages/Index.cshtml.cs
+++ RazorPages1/RazorPages1/Pages/Index.cshtml.cs
@@ -10,18 +10,19 @@ namespace RazorPages1.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    public string Mensaje { get; set; } /*prop mas tab*/
+    public string Mensaje { get; set; } /*prop mas tab, definimos un método nuevo con string en minúscula*/
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger) /*método constructor*/
     {
-        _logger = logger;
+        _logger = logger;/*utilización de dependencias*/
     }
 
     public void OnGet()/*el metodo OnGet que se ejecuta al cargar, antes de 
                         que se abra la página Index se crea un objeto de la clase
                         IndexModel llamado ILogger y luego se ejectua el OnGet*/
     {
-        Mensaje = "Hola 2H";
+        Mensaje = "Hola 2H! Son las " + DateTime.Now.ToLongTimeString();
+        //Se ejecuta el método y luego se abre la pagina, por lo que muestra el mensaje más la hora
     }
 }
 
