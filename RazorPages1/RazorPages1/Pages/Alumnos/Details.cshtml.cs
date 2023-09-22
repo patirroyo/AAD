11e628d1948dacd7cc85dfb3aad1eaa51fcb3afc
Alberto Saz <albertosaz@gmail.com>
2023-09-22 14:02:37 +0200

    Detalles alumno

---
+++ RazorPages1/RazorPages1/Pages/Alumnos/Details.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages.Modelos;
+using RazorPages.Service;
+
+namespace RazorPages1.Pages.Alumnos
+{
+	public class DetailsModel : PageModel
+    {
+        public Alumno alumno { get; set; }
+
+        public DetailsModel(AlumnoRepositorio alumnoRepositorio)
+        {
+            this.alumnoRepositorio = alumnoRepositorio;
+        }
+
+        public void OnGet(int id)
+        {
+            alumno = 
+            
+            
+        }
+    }
+}
