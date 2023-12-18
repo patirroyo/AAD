5e99817b3b13279cd0b9be7bc9ecb526ea1925af
Alberto Saz <albertosaz@gmail.com>
2023-12-18 13:18:04 +0100

    mvc2024

---
+++ MVC2024/MVC2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC2024.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ MVC2024/MVC2024/Models/MarcaModelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MVC2024.Models
+{
+	public class MarcaModelo
+	{
+		public int ID { get; set; }
+		public String Nom_Marca { get; set; }
+		public MarcaModelo()
+		{
+		}
+	}
+}
+
