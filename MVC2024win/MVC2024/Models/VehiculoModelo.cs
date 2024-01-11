5433de4548f93791332eb67a0a120b5169be2147
Alberto Saz <albertosaz@gmail.com>
2024-01-11 08:50:26 +0100

    VehiculoModelo

---
+++ MVC2024win/MVC2024/Models/VehiculoModelo.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC2024.Models
+{
+    public class VehiculoModelo
+    {
+        public int Id { get; set; }
+        public String Matricula { get; set; }
+        public String Color { get; set; }
+        public SerieModelo SerieModelo { get; set; }
+    }
+}
