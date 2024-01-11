64e40718749cc3d06aa111d19b87291836e18cc5
Alberto Saz <albertosaz@gmail.com>
2024-01-11 08:52:48 +0100

    serieId

--- MVC2024win/MVC2024/Models/VehiculoModelo.cs
+++ MVC2024win/MVC2024/Models/VehiculoModelo.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public String Matricula { get; set; }
         public String Color { get; set; }
-        public SerieModelo SerieModelo { get; set; }
+        public SerieModelo Serie { get; set; }
+        public int SerieId {get; set;}
     }
 }
